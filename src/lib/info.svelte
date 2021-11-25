3b1d2fced6b59617a83a2d7ab70a12e307700d51
Kris Alcordo <kalcordo@glimpselive.com>
2021-11-24 22:43:02 -0500

    info area

---
+++ src/lib/info.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<div class="info">
+    <h1>Kris Alcordo</h1>
+    <span>Web Developer </span>
+</div>
+
+<style lang="scss">
+
+</style>
+
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,36 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+    import Header from '$lib/header/Header.svelte';
+    import Info from "$lib/info.svelte"
+    import '../styles/app.css';
+    import '../styles/normalize.css';
+    import '../styles/typography.scss'
 </script>
 
 <Header />
 
 <main>
-	<slot />
+    <section class="mainInfo">
+        <Info />
+    </section>
+
+    <section class="pageContent">
+        <slot />
+    </section>
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 </footer>
 
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+<style lang="scss">
+    main {
+        max-width: 1920px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: minmax(min-content, 300px) 70%;
+        > section {
+            padding: 10px;
+        }
+        .mainInfo {
+        }
+    }
 </style>
